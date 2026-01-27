721ab5b678cf70aec7212b055d1d8eaa3b1a8906
Shreyash0708 <shreyashrpandeypune@gamil.com>
2026-01-27 23:57:34 +0530

    done updation

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,7 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'resources')
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Security settings for production
 if not DEBUG:
