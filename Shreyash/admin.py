bb67ebfc39f7e1b5884da6997183e0a3c873e593
Shreyash0708 <shreyashrpandeypune@gamil.com>
2026-01-28 01:44:49 +0530

    done updation

--- Shreyash/admin.py
+++ Shreyash/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import project, skill
 # Register your models here.
 
-admin.site.register(project)
-admin.site.register(skill)
+# admin.site.register(project)
+# admin.site.register(skill)
